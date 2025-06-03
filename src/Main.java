cc7c7343f68123092babb6032879058f4e36cc09
Mar <dar.marinich@gmail.com>
2025-06-03 21:58:35 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ public class Main {
         int receipt = 300;
         System.out.println(startsum + receipt); // Итого на счету
 
-
         if (receipt > 1000) {
-        System.out.println(receipt / 100); // Бонусы
+            System.out.println(receipt / 100); // Бонусы
         } else {
             System.out.println("0");
         }
